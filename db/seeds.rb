2eaaf4e56ecabcd4ca9c759525e0e5fc4526c1cf
Naomi Yocum <naomiyocum24@gmail.com>
2023-02-02 15:12:23 -0700

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  User.create(user_name: Faker::Name.name, location: Faker::Name.name, email_address: Faker::Name.name)
+  Book.create(isbn: Faker::Name.name, title: Faker::Name.name, author: Faker::Name.name, image_url: Faker::Name.name, page_count: 10, summary: Faker::Name.name)
+end
+
+user = User.first
+book = Book.first
+
+user.books << book
