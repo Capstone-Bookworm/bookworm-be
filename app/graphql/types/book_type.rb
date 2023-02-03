abbbf1b1b5f0b5060faa21b317ffab0fe78ad8c4
Amanda Ross <amikaross@gmail.com>
2023-02-03 14:45:59 -0700

    Add user query type and associated fields

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -11,5 +11,12 @@ module Types
     field :summary, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :borrower, Types::UserType, null: false
+
+    def borrower
+      borrower_id = UserBook.where(user_id: object.user_id).where(book_id: object.id).first.borrower_id
+      User.find(borrower_id)
+    end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -8,5 +8,47 @@ module Types
     field :email_address, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :available_books, [Types::BookType], null: false
+
+    def available_books
+      Book
+        .joins(:user_books)
+        .where("user_books.user_id = #{object.id}")
+        .where("user_books.status = 0")
+    end
+
+    field :pending_requested, [Types::BookType], null: false
+
+    def pending_requested
+      Book.joins(:user_books)
+        .where("user_books.user_id = #{object.id}")
+        .where("user_books.status = 1")
+        .select("books.*, user_books.user_id")
+    end
+
+    field :pending_returned, [Types::BookType], null: false
+
+    def pending_returned
+      Book.joins(:user_books)
+        .where("user_books.user_id = #{object.id}")
+        .where("user_books.status = 2")
+        .select("books.*, user_books.user_id")
+    end
+
+    field :unavailable_books, [Types::BookType], null: false
+
+    def unavailable_books
+      Book.joins(:user_books)
+        .where("user_books.user_id = #{object.id}")
+        .where("user_books.status = 3")
+        .select("books.*, user_books.user_id")
+    end
+
+    field :borrowed_books, [Types::BookType], null: false
+
+    def borrowed_books
+      Book.joins(:user_books).where("user_books.borrower_id = #{object.id}").select("books.*, user_books.id")
+    end
   end
 end
