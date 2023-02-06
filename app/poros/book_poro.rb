955ee454e6c387a395c44efb27bb0f66e018da73
Rich Kaht <adamselene1776@protonmail.com>
2023-02-05 17:00:46 -0700

    Fix: Add sad paths for missing author and image keys

--- app/poros/book_poro.rb
+++ app/poros/book_poro.rb
@@ -9,9 +9,25 @@ class BookPoro
   def initialize(book_data)
     @title = book_data[:volumeInfo][:title]
     @isbn = book_data[:volumeInfo][:industryIdentifiers][0][:identifier]
-    @author = book_data[:volumeInfo][:authors][0]
-    @image_url = book_data[:volumeInfo][:imageLinks][:thumbnail]
+    @author = book_author(book_data)
+    @image_url = book_image(book_data)
     @page_count = book_data[:volumeInfo][:pageCount]
     @summary = book_data[:volumeInfo][:description]
+  end
+
+  def book_author(book_data)
+    if book_data[:volumeInfo][:authors].nil?
+      nil
+    else
+      book_data[:volumeInfo][:authors][0]
+    end
+  end
+
+  def book_image(book_data)
+    if book_data[:volumeInfo][:imageLinks].nil?
+      nil
+    else
+      book_data[:volumeInfo][:imageLinks][:thumbnail]
+    end
   end
 end
