08538d27252eb2d181ec957f03c8c76489876d29
Amanda Ross <amikaross@gmail.com>
2023-02-07 07:22:58 -0700

    Add sad path testing for create user

--- spec/graphql/mutations/users/create_user_spec.rb
+++ spec/graphql/mutations/users/create_user_spec.rb
@@ -14,7 +14,7 @@ module Mutations
 
         it 'returns a User' do 
           post "/graphql", params: { query: happy_query }
-          json = JSON.parse(response.body, symbolize_names: true )
+          json = JSON.parse(response.body, symbolize_names: true)
           data = json[:data]
           user = data[:createUser][:user]
           
@@ -22,6 +22,20 @@ module Mutations
           expect(user[:emailAddress]).to eq('amr@example.com')
           expect(user[:id].to_i).to_not eq(0)
           expect(user[:location]).to eq('Denver')
+        end
+
+        it 'does not create a user and returns an error if attributes are missing' do 
+          expect(User.count).to eq(0)
+
+          post "/graphql", params: { query: sad_query }
+
+          expect(User.count).to eq(0)
+
+          json = JSON.parse(response.body, symbolize_names: true)
+          data = json[:data]
+
+          expect(data[:createUser]).to be(nil)
+          expect(json[:errors][0][:message]).to eq("User name can't be blank, Location can't be blank")
         end
       end
     end
@@ -48,4 +62,25 @@ def happy_query
       }
     }
   GQL
+end
+
+def sad_query
+  <<~GQL
+  mutation {
+    createUser(
+      input: {
+        userName: ""
+        emailAddress: "amr@example.com"
+        location: ""
+      }
+    ) {
+        user {
+        userName
+        emailAddress
+        id
+        location
+      }
+    }
+  }
+GQL
 end
