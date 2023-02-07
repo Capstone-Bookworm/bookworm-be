0faa18c599017605f594ab4c4f4e69336ce10c01
Amanda Ross <amikaross@gmail.com>
2023-02-06 17:57:37 -0700

    Update create_user mutation to get postman test working

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,25 +1,23 @@
-module Mutations
-  class CreateUser < BaseMutation 
-    argument :email_address, String, required: true
-    argument :user_name, String, required: true 
-    argument :location, String, required: true
+class Mutations::CreateUser < Mutations::BaseMutation 
+  argument :email_address, String, required: true
+  argument :user_name, String, required: true 
+  argument :location, String, required: true
+  
+  field :user, Types::UserType, null: false
+  field :errors, [String], null: false
 
-    field :user, Types::UserType, null: false 
-    field :errors, [String], null: false
-
-    def resolve(email_address: nil, user_name: nil, location: nil)
-      user = User.new(email_address: email_address, user_name: user_name, location: location)
-
-      if user.save
-        {
-          user: user
-        }
-      else
-        {
-          user: nil,
-          errors: user.errors.full_messages
-        }
-      end
+  def resolve(email_address:, user_name:, location:)
+    user = User.new(email_address: email_address, user_name: user_name, location: location)
+    if user.save
+      {
+        user: user,
+        errors: []
+      }
+    else 
+      {
+        user: nil,
+        errors: user.errors.full_messages
+      }
     end
   end
 end
