e394f9430e2fb1dfe9298d2a8db27430a788979b
Naomi Yocum <naomiyocum24@gmail.com>
2023-02-07 16:53:58 -0700

    Add testing for borrower field in book query

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -15,8 +15,12 @@ module Types
     field :borrower, Types::UserType, null: false
 
     def borrower
-      borrower_id = UserBook.where(user_id: object.user_id).where(book_id: object.id).first.borrower_id
-      User.find(borrower_id)
+      borrower_id = UserBook.where(book_id: object.id).first.borrower_id
+      if borrower_id.nil?
+        {}
+      else
+        User.find(borrower_id)
+      end
     end
 
     field :users, [Types::UserType], null: false
--- spec/graphql/queries/book_query_spec.rb
+++ spec/graphql/queries/book_query_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Types::QueryType do
   describe 'query book' do
     it 'can query a single book' do
       create_list(:book, 4)
-      book = create(:book, id: 5, isbn: "9780307885166", title: "A Short History Of Nearly Everything", author: "Bill Bryson", image_url: "url", page_count: 876, summary: "This new edition")
+      short_history = create(:book, id: 5, isbn: "9780307885166", title: "A Short History Of Nearly Everything", author: "Bill Bryson", image_url: "url", page_count: 876, summary: "This new edition")
+      user = create(:user)
+      borrower = create(:user)
+      user.books << short_history
+      UserBook.first.update(borrower_id: borrower.id)
 
       query = <<~GQL
         query {
@@ -16,19 +20,61 @@ RSpec.describe Types::QueryType do
             imageUrl
             pageCount
             summary
+            borrower {
+              userName
+            }
+            users {
+              userName
+            }
           }
         }
       GQL
       
-      result = BookwormBeSchema.execute(query)
+      result = BookwormBeSchema.execute(query).to_json
+      json = JSON.parse(result, symbolize_names: true)
+      
+      expect(json[:data][:book][:id]).to eq("5")
+      expect(json[:data][:book][:isbn]).to eq("9780307885166")
+      expect(json[:data][:book][:title]).to eq("A Short History Of Nearly Everything")
+      expect(json[:data][:book][:author]).to eq("Bill Bryson")
+      expect(json[:data][:book][:imageUrl]).to eq("url")
+      expect(json[:data][:book][:pageCount]).to eq(876)
+      expect(json[:data][:book][:summary]).to eq("This new edition")
+      expect(json[:data][:book][:borrower]).to be_a Hash
+      expect(json[:data][:book][:borrower][:userName]).to be_a String
+      expect(json[:data][:book][:users]).to be_an Array
+    end
 
-      expect(result.dig("data", "book", "id")).to eq(String(5))
-      expect(result.dig("data", "book", "isbn")).to eq("9780307885166")
-      expect(result.dig("data", "book", "title")).to eq("A Short History Of Nearly Everything")
-      expect(result.dig("data", "book", "author")).to eq("Bill Bryson")
-      expect(result.dig("data", "book", "imageUrl")).to eq("url")
-      expect(result.dig("data", "book", "pageCount")).to eq(876)
-      expect(result.dig("data", "book", "summary")).to eq("This new edition")
+    it 'returns an empty object if there is no borrower' do
+      short_history = create(:book, id: 5, isbn: "9780307885166", title: "A Short History Of Nearly Everything", author: "Bill Bryson", image_url: "url", page_count: 876, summary: "This new edition")
+      user = create(:user)
+      user.books << short_history
+
+      query = <<~GQL
+        query {
+          book(id:5) {
+            id
+            isbn
+            title
+            author
+            imageUrl
+            pageCount
+            summary
+            borrower {
+              userName
+            }
+            users {
+              userName
+            }
+          }
+        }
+      GQL
+      
+      result = BookwormBeSchema.execute(query).to_json
+      json = JSON.parse(result, symbolize_names: true)
+
+      expect(json[:data][:book][:borrower]).to be_a Hash
+      expect(json[:data][:book][:borrower][:userName]).to be nil
     end
   end
 end
