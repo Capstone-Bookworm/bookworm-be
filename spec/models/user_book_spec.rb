bbbcc559f2d9da6c054f054dc78056d1ca08b5ba
Amanda Ross <amikaross@gmail.com>
2023-02-09 18:39:04 -0700

    Add model testing for delete_ownerless_book callback

--- spec/models/user_book_spec.rb
+++ spec/models/user_book_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe UserBook do
     it {should belong_to :user}
     it {should belong_to :book}
   end
+
+  describe 'instance methods' do 
+    describe '#delete_ownerless_book' do 
+      it 'deletes the associated book after a user_book is destroyed, if that user_book was its last association' do 
+        book = create(:book)
+        user = create(:user)
+        userbook = UserBook.create(user: user, book: book)
+
+        expect(Book.all.length).to eq(1)
+        
+        userbook.destroy
+
+        expect(Book.all.length).to eq(0)
+      end
+    end
+  end
 end
